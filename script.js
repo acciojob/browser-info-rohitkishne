8e494b092b79c0d673426fcbd0d4cb3501efa734
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-07-06 00:14:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const container = document.getElementById("browser-info");
-
+    
 const detail = document.createElement("p")
-details.innerText = `"You are using " + ${navigator.appName()} + " version " + ${navigator.appVersion()}`
-
+detail.innerText = `You are using ${navigator.appName} version ${navigator.appVersion}`
+    
 container.appendChild(detail)
